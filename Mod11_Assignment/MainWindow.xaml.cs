15b1d4d6ea3e0b8613886efe201da06966a833f0
Sophie Green <eng.s.green@gmail.com>
2015-11-30 15:30:05 +0000

    dd

---
+++ Mod11_Assignment/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mod11_Assignment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ArrayList students = new ArrayList();
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreateStudent_Click(object sender, RoutedEventArgs e)
+        {
+            string fn = txtFirstName.Text;
+            string ln = txtLastName.Text;
+            string ct = txtCity.Text;
+           Task task=new Task(() => AddToCollection(fn,ln,ct));
+            ClearForm();
+            task.Start();
+        }
+
+        
+        private void AddToCollection(string fn,string ln,string ct)
+        {
+            // This line of code is intended to simulate network or database latency
+            // It causes a non-responsive UI
+            // Do not remove this line of code as a way of completing the assignment
+            // You MUST use a C# task to get credit
+            Student student = new Student();
+            student.FirstName = fn;
+            student.LastName = ln;
+            student.City = ct;
+            Thread.Sleep(5000);
+            students.Add(student);
+            int count = students.Count;
+            MessageBox.Show("Student created successfully.  Collection contains " + count.ToString() + " Student(s).");
+        }
+
+        private void ClearForm()
+        {
+            txtFirstName.Clear();
+            txtLastName.Clear();
+            txtCity.Clear();
+            txtFirstName.Focus();
+        }
+
+    }
+}
